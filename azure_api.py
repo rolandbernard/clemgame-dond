90728b52ea49f7e2a74a3c6bfabf2336e9f53dea
Roland Bernard <rolli.bernard@gmail.com>
2025-06-03 20:56:10 +0100

    Created some more templates also in other languages.

--- azure_api.py
+++ azure_api.py
@@ -6,17 +6,17 @@ import clemcore.backends.openai_api as openai_api
 
 logger = logging.getLogger(__name__)
 
-NAME = "azure"
+NAME = 'azure'
 
 
 class AzureOpenAI(openai_api.OpenAI):
-    """Backend class for accessing Azure OpenAI remote APIs."""
+    '''Backend class for accessing Azure OpenAI remote APIs.'''
 
     def _make_api_client(self):
         creds = backends.load_credentials(NAME)
         return openai.AzureOpenAI(
-            api_version="2025-04-01-preview",
-            azure_endpoint=creds[NAME]["base_url"],
-            api_key=creds[NAME]["api_key"]
+            api_version='2025-04-01-preview',
+            azure_endpoint=creds[NAME]['base_url'],
+            api_key=creds[NAME]['api_key']
         )
 
