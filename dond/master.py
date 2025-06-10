dfc7a3b8edac1052715088524927547a838687ea
Roland Bernard <rolli.bernard@gmail.com>
2025-06-10 16:49:15 +0100

    Implemented the rest of the game master.

--- dond/master.py
+++ dond/master.py
@@ -63,13 +63,14 @@ class DealOrNoSealPlayer(Player):
             '[Proposal:]', '[Proposal: 2 hats, 1 book]',
         ]
 
-    def _custom_response(self, _):
+    def _custom_response(self, messages):
         word = self._custom_responses.pop(0)
         return f'{word}'
 
 
 @dataclass
 class GameState:
+    mode: str
     max_rounds: int
     player_a_initial_prompt: str
     player_b_initial_prompt: str
@@ -80,11 +81,11 @@ class GameState:
     item_counts: list[int]
     player_a_values: list[int]
     player_b_values: list[int]
-    success: bool = False  # Success is if a compromise is reached.
-    failure: bool = False  # Failure is if proposals are conflicting.
-    aborted: bool = False  # Aborted means the format constraints were broken.
-    proposal_a: list[int] | None = None
-    proposal_b: list[int] | None = None
+    success: bool = False   # Success is if a compromise is reached.
+    failure: bool = False  # Failure is when the proposals were conflicting.
+    aborted: bool = False   # Aborted means the game rules were broken.
+    player_a_proposal: list[int] | None = None
+    player_b_proposal: list[int] | None = None
 
 
 class DealOrNoDeal(DialogueGameMaster):
@@ -125,6 +126,7 @@ class DealOrNoDeal(DialogueGameMaster):
         self.add_player(self.player_b, initial_prompt=player_b_initial_prompt)
         # Named arguments to avoid any order sensitivity.
         self.state = GameState(
+            mode=self.experiment['mode'],
             max_rounds=self.experiment['max_turns'],
             player_a_initial_prompt=player_b_initial_prompt,
             player_b_initial_prompt=player_b_initial_prompt,
@@ -145,7 +147,10 @@ class DealOrNoDeal(DialogueGameMaster):
         }[self.experiment['language']])
 
     def _does_game_proceed(self):
-        return not (self.state.aborted or self.state.failure or self.state.success)
+        return not (
+            self.state.aborted or self.state.aborted
+            or self.state.failure or self.state.success
+        )
 
     def _parse_response(self, player: Player, response: str) -> str | list[int]:
         # Check if the message contains a proposal.
@@ -190,3 +195,87 @@ class DealOrNoDeal(DialogueGameMaster):
             # If this is not a proposal, the message must not be parsed. The
             # players can communicate however they want.
             return response
+
+    def _on_parse_error(self, error: ParseError):
+        self.log_to_self('invalid format', 'abort game')
+        self.state.aborted = True
+
+    def _advance_game(self, player: Player, parsed_response: str | list[int]):
+        # If this is a string, the message was not a proposal.
+        if isinstance(parsed_response, str):
+            if self.current_round == self.state.max_rounds \
+                or self.state.player_a_proposal is not None \
+                    or self.state.player_b_proposal is not None:
+                # In these cases we expect a proposal. This is therefore a rule
+                # violation.
+                raise RuleViolationError(
+                    f'player was instructed to make a proposal', parsed_response
+                )
+            self.log_to_self('valid message', parsed_response)
+            if player == self.player_a:
+                self.set_context_for(self.player_b, parsed_response)
+            else:
+                assert player == self.player_b
+                # If this was the last allowed turn, we prompt the next player to
+                # make their secret proposal.
+                self.set_context_for(
+                    self.player_a, parsed_response + '\n\n' +
+                    self.state.proposal_prompt_timeout
+                    if self.current_round == self.state.max_rounds - 1 else parsed_response
+                )
+        else:
+            if any(
+                proposed > count for count, proposed in zip(self.state.item_counts, parsed_response)
+            ):
+                # This was parsed correctly, but the user specified to many items.
+                # We could count this as either a rule violation or a failed
+                # proposal. I will count it as a rule violation.
+                raise RuleViolationError(
+                    'proposal includes more items than available',
+                    str(parsed_response)
+                )
+            self.log_to_self('valid proposal', parsed_response)
+            if player == self.player_a:
+                self.state.player_a_proposal = parsed_response
+                # Will be ignored if this is the end.
+                self.set_context_for(
+                    self.player_b, self.state.proposal_prompt_early
+                )
+            else:
+                assert player == self.player_b
+                self.state.player_b_proposal = parsed_response
+                # Will be ignored if this is the end.
+                self.set_context_for(
+                    self.player_a, self.state.proposal_prompt_early
+                )
+            if self.state.player_a_proposal is not None \
+                    and self.state.player_b_proposal is not None:
+                if any(
+                    # The sum of proposed items must be less than the available count.
+                    cnta + cntb > count for count, cnta, cntb
+                    in zip(self.state.item_counts, self.state.player_a_proposal, self.state.player_b_proposal)
+                ):
+                    # Record that the agreement failed.
+                    self.log_to_self(
+                        'failed agreement',
+                        (self.state.player_a_proposal,
+                         self.state.player_b_proposal)
+                    )
+                    self.state.failure = True
+                else:
+                    # Record that there was a successful agreement.
+                    self.log_to_self(
+                        'successful agreement',
+                        (self.state.player_a_proposal,
+                         self.state.player_b_proposal)
+                    )
+                    self.state.success = True
+
+    def _on_game_error(self, error: GameError):
+        self.log_to_self('error', error.reason)
+        self.state.aborted = True
+
+    def _on_after_game(self):
+        self.log_key(METRIC_ABORTED, int(self.state.aborted))
+        self.log_key(METRIC_LOSE, int(self.state.failure))
+        self.log_key(METRIC_SUCCESS, int(self.state.success))
