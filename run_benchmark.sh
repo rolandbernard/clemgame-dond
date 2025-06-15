680819fc0921eca79e39ec81c02f70db548bc768
Roland Bernard <rolli.bernard@gmail.com>
2025-06-15 02:36:12 +0200

    Removed also the magistral models.

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -15,12 +15,10 @@ models=(
     "mistral-small-2503"
     "llama-3-70b"
     "gpt-3.5-turbo-0125"
-    "magistral-small-2506"
     "gemini-2.0-flash-001"
     "llama-4-maverick-17b-128e"
     "mistral-large-2411"
     "o4-mini-2025-04-16"
-    "magistral-medium-2506"
     "gpt-4.1-2025-04-16"
     "gemini-2.5-flash-preview-05-20"
     "gpt-4-turbo-2024-04-09"
@@ -30,6 +28,8 @@ models=(
 # off.
 # "qwen3-32b"
 # "qwen-qwq-32b"
+# "magistral-small-2506"
+# "magistral-medium-2506"
 
 languages=("en" "de" "it")
 
